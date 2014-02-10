347d606b11e7b29251f789860be4b85fdb7b848e
Dennis Roberts <dennis@iplantcollaborative.org>
2014-02-10 15:22:12 -0700

    CORE-5133: added some fields that are required by Sonatype to project.clj

--- project.clj
+++ project.clj
@@ -1,16 +1,24 @@
-(defproject proboscis "0.2.0-SNAPSHOT"
+(defproject proboscis "0.2.1-SNAPSHOT"
   :description "A utility for creating an ElasticSearch index and its mappings for Infosquito."
   :url "http://www.iplantcollaborative.org"
   :license {:name "BSD License"
             :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
+  :scm {:connection "scm:git:git@github.com:iPlantCollaborativeOpenSource/proboscis.git"
+        :developerConnection "scm:git:git@github.com:iPlantCollaborativeOpenSource/proboscis.git"
+        :url "git@github.com:iPlantCollaborativeOpenSource/proboscis.git"}
+  :pom-addition [:developers
+                 [:developer
+                  [:url "https://github.com/orgs/iPlantCollaborativeOpenSource/teams/iplant-devs"]]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.cli "0.2.2"]
                  [cheshire "5.2.0"]
                  [clojurewerkz/elastisch "1.2.0"]
                  [slingshot "0.10.3"]]
   :resource-paths ["config"]
-  :plugins [[org.iplantc/lein-iplant-cmdtar "0.1.0-SNAPSHOT"]]
-  :repositories [["iplantCollaborative"
-                  "http://projects.iplantcollaborative.org/archiva/repository/internal/"]]
+  :plugins [[org.iplantc/lein-iplant-cmdtar "0.1.2-SNAPSHOT"]]
+  :repositories [["sonatype-nexus-snapshots"
+                  {:url "https://oss.sonatype.org/content/repositories/snapshots"}]]
+  :deploy-repositories [["sonatype-nexus-staging"
+                         {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"}]]
   :aot [proboscis.core]
   :main proboscis.core)
